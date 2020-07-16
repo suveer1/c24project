4880e585047f263b752321898a393bd638ffc7a4
suveer1 <67051835+suveer1@users.noreply.github.com>
2020-07-16 15:35:17 +0530

    Add files via upload

---
+++ papperclass.js
@@ -0,0 +1,20 @@
+class Papper{
+    constructor(x,y){
+    var options ={
+        isStatic:false,
+        restitution:0.3,
+        friction:0.5,
+        density:1.2
+    }
+    this.body=Bodies.circle(x,y,20,options);
+    this.radius=20;
+    World.add(world,this.body)
+    }
+    display(){
+        var pos = this.body.position;
+        fill("blue");
+        ellipseMode(CENTER);
+        ellipse(pos.x,pos.y,this.radius);
+    }
+
+}
